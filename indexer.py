c70b115c0a4b0a640d749e271eeb4e339e16324e
Aman Khosa <amankhoza@hotmail.co.uk>
2017-03-27 14:11:59 +0100

    Skeleton for indexer

---
+++ indexer.py
@@ -0,0 +1,68 @@
+import sys
+import os
+from bs4 import BeautifulSoup
+from bs4 import Comment
+from bs4 import Doctype
+
+
+def progress(count, total, status='Complete'):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+
+def getHTMLFilePaths(directory):
+    htmlFilePaths = []
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            if name.endswith('.html'):
+                path = os.path.join(root, name)
+                htmlFilePaths.append(path)
+    return htmlFilePaths
+
+
+def removeJunk(soup):
+    comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+    [comment.extract() for comment in comments]  # remove comments
+    [doctype.extract() for doctype in soup.contents if isinstance(doctype, Doctype)]  # remove doctypes
+    [script.extract() for script in soup('script')]  # remove scripts
+
+
+def htmlToText(soup):
+    texts = soup.findAll(text=True)
+    text = ' '.join([item.strip() for item in texts])
+    return text
+
+
+def retrieveLinksInHTML(soup):
+    links = soup.find_all('a', href=True)
+    return [link['href'] for link in links]  # might need to tweak to remove mailtos
+
+
+os.system('reset')  # clear terminal
+
+filePaths = getHTMLFilePaths('data')
+
+i = 1
+n = len(filePaths)
+
+for filePath in filePaths:
+    progress(i, n)
+    i += 1
+
+    file = open(filePath)
+    soup = BeautifulSoup(file, 'lxml')
+
+    removeJunk(soup)
+
+    text = htmlToText(soup)
+    links = retrieveLinksInHTML(soup)
+
+    # print(filePath)
+    # print(text)
+    # print(links)
+
+    file.close()
