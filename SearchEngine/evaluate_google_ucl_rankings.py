fd1d26ee112543887f6718b34b49200d21e806df
Aman Khosa <amankhoza@hotmail.co.uk>
2017-04-20 10:34:13 +0100

    Add evaluation script for Google and UCL search results

---
+++ SearchEngine/evaluate_google_ucl_rankings.py
@@ -0,0 +1,30 @@
+from __future__ import division
+from utils import check_python_version
+from evaluate_rankings import normalised_discounted_cumulative_gain
+import sys
+
+DELIMITER = ',###,'
+
+check_python_version()
+
+eval_file_path = sys.argv[1]
+
+search_eval_file = open(eval_file_path, 'r')
+
+headers = search_eval_file.readline()  # ignore headers
+
+ndcgs = []
+
+for line in search_eval_file:
+    split_line = line.strip().split(DELIMITER)
+    query = split_line[0]
+    relevancies_string = split_line[2]
+    relevancies = list(map(int, relevancies_string.split(',')))
+    indexes = range(1, 11)
+    ndcg = normalised_discounted_cumulative_gain(indexes, relevancies)
+    if ndcg >= 0:
+        ndcgs.append(ndcg)
+        print(str(ndcg)+'\t'+query)
+
+avg_ndcg = sum(ndcgs) / len(ndcgs)
+print('Avg ndcg for '+eval_file_path.split('.csv')[0]+' : '+str(avg_ndcg))
